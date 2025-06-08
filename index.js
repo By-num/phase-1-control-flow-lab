b35e0f9adc0818e6f6cd1d1924ec5a459400cb79
By-num <bynumst3cy@gmail.com>
2025-06-08 18:50:59 +0300

    CompleteControl Flow lab

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet()
   // Write your code here!
-}
-
+  if (someValue <=400 ){
+    return ("This is on me");
+  }else if (someValue >400 $$ somevalue <=2000 ){
+    return ("that will be 20 bucks");
+  }else is (someValue >2000){
+    return ("I will gladly take your thirty bucks.")
+  }else{
+    return ("No can do.")
+  }
 function ternaryCheckCity(){
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  switch (tip){
+    case "Generous":
+return "thank you so much";
+    case "not as generous":
+      return "Thank you"
+      
+      default:
+        return "bye"
+  }
 }
